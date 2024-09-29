7d138296460ebf9bb3cebfa0fcd763449e30bd6c
ketan Dogra <ketandogra1367@gmail.com>
2024-09-29 21:39:04 +0530

    setup inittial project with jenkins file

---
+++ devops_day1/app.js
@@ -0,0 +1,13 @@
+// app.js
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('Hello World');
+
+});
+
+module.exports = app;
---
+++ devops_day1/server.js
@@ -0,0 +1,7 @@
+// server.js
+
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
